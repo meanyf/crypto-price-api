a2abd10c3ab5db3d1a15fcfc58a8b97393846929
Amir <meanyf@yandex.ru>
2025-10-29 10:32:31 +0300

    add post for add crypto

--- app/api/v1/endpoints/crypto.py
+++ app/api/v1/endpoints/crypto.py
@@ -1,24 +1,33 @@
 # crypto.py
 
-from fastapi import APIRouter, Depends
-from app.services.crypto_service import get_top_cryptos, get_crypto_list, get_crypto_price
+from fastapi import APIRouter, Depends, Form, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from app.services.crypto_service import set_crypto_mapping, get_crypto_price, add_crypto
 from app.adapters.coingecko_adapter import CoinGeckoAdapter
 from typing import Annotated
 from app.schemas.user import User
 from app.api.deps import get_current_user, get_coingecko_client
 crypto_router = APIRouter(prefix="/crypto", tags=["crypto"])
 
-@crypto_router.get("/")
-async def list_cryptos(current_user: Annotated[User, Depends(get_current_user)], client: CoinGeckoAdapter = Depends(get_coingecko_client)):
-    return await get_top_cryptos(client)
+# @crypto_router.get("/")
+# async def list_cryptos(current_user: Annotated[User, Depends(get_current_user)], client: CoinGeckoAdapter = Depends(get_coingecko_client)):
+#     return await get_top_cryptos(client)
 
 
-@crypto_router.get("/list")
-async def list_cryptos(
-    current_user: Annotated[User, Depends(get_current_user)],
-    client: CoinGeckoAdapter = Depends(get_coingecko_client),
-):
-    return await get_crypto_list(client)
+# @crypto_router.get("/list")
+# async def list_cryptos(
+#     current_user: Annotated[User, Depends(get_current_user)],
+#     client: CoinGeckoAdapter = Depends(get_coingecko_client),
+# ):
+#     return await set_crypto_mapping(client)
+
+templates = Jinja2Templates(directory="templates")
+
+
+@crypto_router.get("/", response_class=HTMLResponse)
+def index(request: Request):
+    return templates.TemplateResponse("users/crypto.html", {"request": request})
 
 
 @crypto_router.get("/{symbol}")  # GET /crypto/{symbol}
@@ -28,3 +37,12 @@ async def crypto_symbol(
     client: CoinGeckoAdapter = Depends(get_coingecko_client),
 ):
     return await get_crypto_price(client, symbol)
+
+
+@crypto_router.post("/")
+async def add_symbol(
+    symbol: Annotated[str, Form()],  
+    current_user: Annotated[User, Depends(get_current_user)],
+    client: CoinGeckoAdapter = Depends(get_coingecko_client),
+):
+    return await add_crypto(client, symbol)
--- app/services/crypto_service.py
+++ app/services/crypto_service.py
@@ -6,44 +6,47 @@ from sqlalchemy.orm import Session
 from app.db.models import User
 from app.db.base import cache
 
-async def get_top_cryptos(client: CoingeckoPort) -> List[dict]:
-    data = await client.fetch_markets()
-    return [
-        {
-            "symbol": coin["symbol"].upper(),
-            "name": coin["name"],
-            "current_price": coin["current_price"],
-            "last_updated": coin["last_updated"],
-        }
-        for coin in data
-    ]
+# async def get_top_cryptos(client: CoingeckoPort) -> List[dict]:
+#     data = await client.fetch_markets()
+#     return [
+#         {
+#             "symbol": coin["symbol"].upper(),
+#             "name": coin["name"],
+#             "current_price": coin["current_price"],
+#             "last_updated": coin["last_updated"],
+#         }
+#         for coin in data
+#     ]
 
 
-async def get_crypto_list(client: CoingeckoPort) -> List[dict]:
+async def set_crypto_mapping(client: CoingeckoPort) -> List[dict]:
+    from itertools import islice
     data = await client.fetch_crypto_list()
+    print(data[:3])
     for item in data:
         key = item['symbol']
         cache[key] = {"id": item["id"], "name": item["name"]}
+    print(dict(islice(cache.items(), 3)))
     return cache
 
 
-async def get_crypto_price(client: CoingeckoPort, crypto_name) -> List[dict]:
-    data = await client.fetch_crypto_price(cache[crypto_name]['id'])
+async def get_crypto_price(client: CoingeckoPort, crypto_symbol) -> List[dict]:
+    if not cache:
+        await set_crypto_mapping(client)
+    data = await client.fetch_crypto_price(cache[crypto_symbol]['id'])
     return data
 
 
 async def add_crypto(
-    db: Session,
-    client: CoingeckoPort,
-    username: str,
+    client: CoingeckoPort, crypto_symbol: str
 ) -> List[dict]:
-    data = await client.fetch_markets()
-    return [
-        {
-            "symbol": coin["symbol"].upper(),
-            "name": coin["name"],
-            "current_price": coin["current_price"],
-            "last_updated": coin["last_updated"],
-        }
-        for coin in data
-    ]
+    if not cache:
+        await set_crypto_mapping(client)
+    d = dict()
+    id = cache[crypto_symbol]["id"]
+    data = await client.fetch_crypto_price(id)
+    d["symbol"] = crypto_symbol
+    d["name"] = cache[crypto_symbol]['name']
+    d['price'] = data[id]['usd']
+    # d["last_updated"] = cache[crypto_symbol]["id"]["last_updated"]
+    return d
